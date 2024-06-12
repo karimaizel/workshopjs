ffea888d126df96c96514d872f0938903bc7e62a
karimaizel <karim.aizel4321@gmail.com>
2024-06-12 16:40:55 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,139 @@
+
+
+
+
+
+
+// var sentence = "hello i want to reverse you"
+//  console.log(sentence.split("").reverse().join(""))
+
+// __________________________________________________________
+
+
+// var p1 = {
+//     dievalue :0,
+//     position : 1,
+// }
+
+// var p2 = {
+//     dievalue :0,
+//     position : 1,
+// }
+// var bonus = 0
+// var ispossible = false
+
+
+// do{
+//       do{
+//         p1.dievalue =prompt('player1: enter between 1 and 6')
+//         p2.dievalue =prompt('player2: enter between 1 and 6')
+//       }
+//     while(p1.dievalue<1 || p1.dievalue>6 || p2.dievalue<1 || p2.dievalue>6)
+//     p1.position+=parseInt(p1.dievalue)
+//     console.log("player1 position"+ p1.position)
+//     p2.position+=parseInt(p2.dievalue)
+//     console.log("player2 position"+ p2.position)
+
+// if(p1.position==p2.position){
+//     ispossible=true
+//     bonus++
+//     console.log('bonus gained')
+// }
+// else if (Math.abs(parseInt(p1.dievalue))-(parseInt(p2.dievalue)) <=6 ){
+//     ispossible=true
+//     console.log('possible match')
+// }
+// }
+// while(p1.position<9 && p2.position<9)
+// console.log('end game')
+// _______________________________________________________________________
+
+
+// base=prompt("enter the base of the triangle in cm")
+// hight=prompt("enter the height of the triangle in cm")
+// function area(base,hight){
+//     return  base*hight/2 ;    
+// }
+// let result = area(base,hight)
+
+// if(base>0 && hight>0){
+//         console.log("the area of this triangle is:"+ result + "cm2")
+// }
+// else{
+//     console.log("the area of this triangle is: 0 cm2 ")
+// }
+
+// _____________________________________________________________________________
+// var arr1 = [2,10]
+// var arr2 = [3,4,5,6,7,8,9]
+
+// function tuckIn(arr1, arr2) {
+// 	return  [arr1[i=0] , ...arr2 ,arr1[i=1]]          
+// }
+
+// let arr3 = tuckIn(arr1, arr2)
+// console.log(arr3)
+
+// ___________________________________________________________________________________
+// var p1 = {
+//     a : 1
+//     b : 0
+// }
+// var p2 = {
+//     a : 1
+//     b : 0
+// }
+
+// let (b <=6)
+
+// if(parseInt(p1.b)-parseInt(p2.b)<=6){
+//     possibleBonus == true
+//     function possibleBonus(a,b){
+//         p1.a +=p1.b
+//         p2.a+=p2.b
+// }
+// } 
+//  else{
+//     function possibleBonus(a,b){
+//         p1.a +=p1.b
+//         p2.a+=p2.b
+  
+
+//     possibleBonus == false 
+// }
+// }   
+// console.log(possibleBonus)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+// if(parseInt (dievalueA)-parseInt(dievalueB)<=6 || parseInt (dievalueB)-parseInt(dievalueA)<=6)  {
+// function possibleBonus(a, b) {
+// 	a+=dievalueA
+//     b+=dievalueB
+// }
+// possibleBonus(a, b)== true
+// console.log(possibleBonus)
+// }
+// else{
+// possibleBonus(a, b)== false
+// console.log(possibleBonus)
+// }
